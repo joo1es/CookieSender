7d924017e19506d1d6013f1278afb85386cb763c
jooies <jooies@qq.com>
2023-02-07 18:56:05 +0800

    feat: finish

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener(async (msg, sender, sendResponse) => {
     if (msg.topic === 'sendCookies') {
         const cookie = await getAllCookies(msg)
-        const url = new URL('https://kan.tu/api/set-cookie')
+        const url = new URL('https://kan.tu/cookie.api')
         url.searchParams.set('domain', msg.domain)
         url.searchParams.set('content', cookie)
         await fetch(url)
